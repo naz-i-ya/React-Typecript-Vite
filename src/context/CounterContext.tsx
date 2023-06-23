9fd4739222cbb0163cba1434f922736b22bcc76e
naz-i-ya <naziyanaz1208@gmail.com>
2023-06-24 01:42:10 +0530

    UseContext

---
+++ src/context/CounterContext.tsx
@@ -0,0 +1,72 @@
+import { createContext, useReducer, ChangeEvent, ReactElement, useCallback } from "react";
+
+type StateType = {
+    count: number;
+    text: string;
+}
+
+export const initState: StateType = { count: 0, text: ''}
+
+const enum REDUCER_ACTION_TYPE {
+    INCREMENT,
+    DECREMENT,
+    NEW_INPUT
+}
+
+type ReducerAction = {
+    type: REDUCER_ACTION_TYPE,
+    payload?: string
+}
+
+const reducer = (state: StateType, action: ReducerAction): StateType => {
+    switch (action.type){
+        case REDUCER_ACTION_TYPE.INCREMENT:
+            return {...state, count: state.count + 1}
+
+        case REDUCER_ACTION_TYPE.DECREMENT: 
+        return {...state, count: state.count - 1}
+
+        case REDUCER_ACTION_TYPE.NEW_INPUT: 
+        return {...state, text: action.payload ?? ''}
+        default:
+            throw new Error()
+    }
+}
+
+const useCounterContext = (initSTate: StateType) => {
+    const [state, dispatch] = useReducer(reducer, initState)
+
+    const increment = useCallback(() => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT}), [])
+
+    const decrement = useCallback(() => dispatch( {type: REDUCER_ACTION_TYPE.INCREMENT}), [])
+
+    const handleTextInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({ type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value})
+}, [])
+return { state, increment, decrement, handleTextInput }
+}
+
+type UseCounterContextType = ReturnType<typeof useCounterContext>
+
+const initContextState: UseCounterContextType = {
+    state: initState,
+    increment: () => {},
+    decrement: () => {},
+    handleTextInput: (e: ChangeEvent<HTMLInputElement>) => {},
+}
+
+export const CounterContext = createContext<useCounterContextType>(initContextState)
+
+type ChildrenType = {
+    children?: ReactElement | undefined
+}
+
+export const CounterProvider = ({
+    children, ...initState
+}: ChildrenType & StateType): ReactElement => {
+    return (
+        <CounterContext.Provider value={useCounterContext(initState)}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
